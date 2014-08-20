070271c92be4b15583788a498ae9e7a4b2cc14b2
anli5005 <li.anthony917@gmail.com>
2014-08-20 11:41:32 -0400

    Update login.lua
    Improved the DLogged variable

--- System/Core/Login.app/login.lua
+++ System/Core/Login.app/login.lua
@@ -1,11 +1,11 @@
 local args = {...}
-DLogged = args[1]
+shell.DLogged = args[1]
 term.setBackgroundColor(colors.gray)
 term.setTextColor(colors.lightGray)
 term.clear()
 term.setCursorPos(1,1)
 print("Diamond")
 print(" ")
-print(" Welcome, "..DLogged.."!")
+print(" Welcome, "..shell.DLogged.."!")
 shell.setDir("")
 shell.run("Diamond/System/Core/Home.app/main.lua")
