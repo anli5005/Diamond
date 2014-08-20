2d690f32987d321412ea16165fffc7be13598a79
anli5005 <li.anthony917@gmail.com>
2014-08-20 12:39:01 -0400

    Tried to fix DLogged bug

--- System/Core/Login.app/login.lua
+++ System/Core/Login.app/login.lua
@@ -1,11 +1,11 @@
 local args = {...}
-DLogged = args[1]
+multishell.DLogged = args[1]
 term.setBackgroundColor(colors.gray)
 term.setTextColor(colors.lightGray)
 term.clear()
 term.setCursorPos(1,1)
 print("Diamond")
 print(" ")
-print(" Welcome, "..DLogged.."!")
+print(" Welcome, "..multishell.DLogged.."!")
 shell.setDir("")
 shell.run("Diamond/System/Core/Home.app/main.lua")
