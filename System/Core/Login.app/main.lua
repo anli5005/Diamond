90d689129a470a997a7bf25ccf4986282aa07551
anli5005 <li.anthony917@gmail.com>
2014-08-22 09:12:35 -0400

    Prepared login for passwords

--- System/Core/Login.app/main.lua
+++ System/Core/Login.app/main.lua
@@ -33,51 +33,52 @@ local function checkForUsers()
     return {}
   end
 end
-local selected
-local theUsers = checkForUsers()
-if #theUsers == 0 then
-  term.setCursorPos(2,6)
-  term.write("Welcome!")
-  term.setCursorPos(2,7)
-  print("Please type in a new username:")
-  print(" (You can set a password in Settings later)")
-  write(" ")
-  local theNewName
-  while true do
-    local newName = read()
-    if (newName == "") or (string.sub(newName, 1, 1) == ".") then
-      local x,y = term.getCursorPos()
-      term.setCursorPos(2,y-1)
-      term.clearLine()
-    else
-      theNewName = newName
-      break
-    end
-  end
-  if fs.exists(fs.combine("/Users", theNewName)) then
-    fs.delete(fs.combine("/Users", theNewName))
-  end
-  fs.makeDir(fs.combine("/Users/"..theNewName, "Diamond"))
-  selected = theNewName
-else
-  term.setCursorPos(1,6)
-  for theUserKey,theUserName in ipairs(theUsers) do
+while true do
+  local selected
+  local theUsers = checkForUsers()
+  if #theUsers == 0 then
+    term.setCursorPos(2,6)
+    term.write("Welcome!")
+    term.setCursorPos(2,7)
+    print("Please type in a new username:")
+    print(" (You can set a password in Settings later)")
     write(" ")
-    print(theUserName)
-  end
-  print(" Hold CTRL+S to shutdown")
-  print(" Hold CTRL+R to restart")
-  term.setTextColor(colors.lightGray)
-  term.setCursorBlink(true)
-  while true do
-    local e, btn, x, y = os.pullEvent("mouse_click")
-    if theUsers[y-5] then
-      selected = theUsers[y-5]
+    local theNewName
+    while true do
+      local newName = read()
+      if (newName == "") or (string.sub(newName, 1, 1) == ".") then
+        local x,y = term.getCursorPos()
+        term.setCursorPos(2,y-1)
+        term.clearLine()
+      else
+        theNewName = newName
+        break
+      end
+    end
+    if fs.exists(fs.combine("/Users", theNewName)) then
+      fs.delete(fs.combine("/Users", theNewName))
+    end
+    fs.makeDir(fs.combine("/Users/"..theNewName, "Diamond"))
+    selected = theNewName
+  else
+    term.setCursorPos(1,6)
+    for theUserKey,theUserName in ipairs(theUsers) do
+      write(" ")
+      print(theUserName)
+    end
+    print(" Hold CTRL+S to shutdown")
+    print(" Hold CTRL+R to restart")
+    term.setTextColor(colors.lightGray)
+    term.setCursorBlink(true)
+    while true do
+      local e, btn, x, y = os.pullEvent("mouse_click")
+      if theUsers[y-5] then
+        selected = theUsers[y-5]
       break
+      end
     end
   end
+  local appPath = fs.getDir(shell.getRunningProgram())
+  shell.setDir("")
+  shell.run(fs.combine(appPath, "login.lua"), selected)
 end
-term.setCursorBlink(false)
-local appPath = fs.getDir(shell.getRunningProgram())
-shell.setDir("")
-shell.run(fs.combine(appPath, "login.lua"), selected)
