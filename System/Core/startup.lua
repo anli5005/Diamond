398cfa3de865cca877a3fe513c193a90d8ac77f7
anli5005 <li.anthony917@gmail.com>
2014-08-25 16:02:32 -0400

    Update startup.lua

--- System/Core/startup.lua
+++ System/Core/startup.lua
@@ -3,6 +3,8 @@ DSystemPath = fs.getDir(fs.getDir(fs.getDir(shell.getRunningProgram())))
 term.setBackgroundColor(colors.lightBlue)
 term.clear()
 
+os.loadAPI(fs.combine(DSystemPath, "System/APIs/D"))
+
 local loginPath = fs.combine(DSystemPath, "System/Core/Login.app/main.lua")
 shell.setDir("/")
 shell.run(loginPath)
