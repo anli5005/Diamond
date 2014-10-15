c454841df6d1cc74c41011c93d203ff7ba97422e
anli5005 <li.anthony917@gmail.com>
2014-10-14 21:18:59 -0400

    Update API loading

--- System/Core/startup.lua
+++ System/Core/startup.lua
@@ -3,7 +3,7 @@ DSystemPath = fs.getDir(fs.getDir(fs.getDir(shell.getRunningProgram())))
 term.setBackgroundColor(colors.lightBlue)
 term.clear()
 
-os.loadAPI(fs.combine(DSystemPath, "System/APIs/D"))
+os.loadAPI(fs.combine(DSystemPath, "System/APIs/S"))
 
 local loginPath = fs.combine(DSystemPath, "System/Core/Login/main.lua")
 shell.setDir("/")
